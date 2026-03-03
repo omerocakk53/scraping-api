b479887eb4c34c89b5903c89058a4811a2b99917
Ömer Ocak <ocakomer53@gmail.com>
2026-03-03 20:52:19 +0300

    fix: linuxda böyle çalışırmış

--- src/services/browserService.js
+++ src/services/browserService.js
@@ -5,8 +5,13 @@ let browser;
 const getBrowser = async () => {
   if (!browser) {
     browser = await puppeteer.launch({
-      headless: "new",
-      args: ["--no-sandbox", "--disable-setuid-sandbox"],
+      headless: true,
+      args: [
+        "--no-sandbox",
+        "--disable-setuid-sandbox",
+        "--disable-dev-shm-usage",
+        "--disable-gpu",
+      ],
     });
     console.log("Browser launched");
   }
